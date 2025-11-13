0c3d3a96b19868e2174e02988287500bbd679766
Joachim Jansen <jofelbi66@gmail.com>
2025-11-13 11:53:32 +0100

    Update checklist-data.js

--- checklist-data.js
+++ checklist-data.js
@@ -1,1 +1,12 @@
+// checklist-data.js
+// Hier liegen alle Inhalte der täglichen Mobilkran-Kontrolle
+
+const dailyMobileCraneCheck = {
+  id: "daily-mobile-crane-check",
+  title: "Tägliche Kontrolle Mobilkran",
+  version: "2025-11-01",
+  sections: [
+    // 👉 HIER fügst du jetzt alle Sections + Items ein
+  ]
+};
 
